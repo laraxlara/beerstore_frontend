417b98ef0ec03c64885208ee8332e144b975d5b8
laraxlara <107651453+laraxlara@users.noreply.github.com>
2022-12-30 11:59:45 +0100

    key

--- src/pages/products/ipa.js
+++ src/pages/products/ipa.js
@@ -21,18 +21,18 @@ const Ipa = () => {
                     {products && products.map((product) => {
                         if (product.type === 'IPA') {
                         return (
-                            <div className="flex flex-col md:flex-row items-center border rounded-lg overflow-hidden">
+                            <div key={product.id} className="flex flex-col md:flex-row items-center border rounded-lg overflow-hidden">
                                 <Link to="/" className="group w-full md:w-32 lg:w-48 h-48 md:h-full block self-start shrink-0 bg-gray-100 overflow-hidden relative">
                                     <img src={imgimg} loading="lazy" alt="Product" className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200" />
                                 </Link>
                                 <div className="flex flex-col gap-2 p-4 lg:p-6">
                                     <span className="text-gray-400 text-sm">{product.type}</span>
                                     <h2 className="text-gray-800 text-xl font-bold">
-                                        <Link to="/" className="hover:text-indigo-500 active:text-indigo-600 transition duration-100">New trends in Tech</Link>
+                                        <Link to="/" className="hover:text-orange-700 active:text-orange-500 transition duration-100">New trends in Tech</Link>
                                     </h2>
                                     <p className="text-gray-500">This is a section of some simple filler text, also known as placeholder text.</p>
                                     <div>
-                                        <Link to="/" className="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100">Read more</Link>
+                                        <Link to="/" className="text-orange-700 hover:text-orange-500 active:text-orange-600 font-semibold transition duration-100">Read more</Link>
                                     </div>
                                 </div>
                             </div>
